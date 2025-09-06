6ac61f58b6852c4d9465f2dd376e4039559b0353
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-09-06 20:09:50 +0800

    Add files via upload

---
+++ Models/TblTeacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MachineProblem1.Models;
+
+public partial class TblTeacher
+{
+    public int Id { get; set; }
+
+    public string? TeacherName { get; set; }
+
+    public int? YearOfExperience { get; set; }
+}

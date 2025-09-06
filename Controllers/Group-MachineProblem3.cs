7e7a25bc1ef4da6dadc44ace61b33f9542c0d4f7
Jsuckava <pascualpatrick264@gmail.com>
2025-09-06 20:06:10 +0800

    Controllers to tas Model

---
+++ Controllers/Group-MachineProblem3.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MachineProblem1.Models;
+
+namespace MachineProblem3.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CrudforMachineProb3 : ControllerBase
+{
+    private readonly TeachersDbContext _context;
+
+    public CrudforMachineProb3(TeachersDbContext context) => _context = context;
+
+    [HttpGet]
+    public async Task<IEnumerable<TblTeacher>> GetAll() =>
+        await _context.TblTeachers.ToListAsync();
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TblTeacher>> GetById(int id)
+    {
+        var teacher = await _context.TblTeachers.FindAsync(id);
+        return teacher is null ? NotFound() : teacher;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TblTeacher>> Create(TblTeacher teacher)
+    {
+        _context.TblTeachers.Add(teacher);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetById), new { id = teacher.Id }, teacher);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, TblTeacher teacher)
+    {
+        if (id != teacher.Id) return BadRequest();
+        _context.Entry(teacher).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var teacher = await _context.TblTeachers.FindAsync(id);
+        if (teacher is null) return NotFound();
+        _context.TblTeachers.Remove(teacher);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ Models/TeachersDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachineProblem1.Models;
+
+public partial class TeachersDbContext : DbContext
+{
+    public TeachersDbContext()
+    {
+    }
+
+    public TeachersDbContext(DbContextOptions<TeachersDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblTeacher> TblTeachers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=TeachersDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblTeacher>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tbl_Teac__3214EC07C0D7EBF2");
+
+            entity.ToTable("tbl_Teachers");
+
+            entity.Property(e => e.TeacherName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("teacherName");
+            entity.Property(e => e.YearOfExperience).HasColumnName("yearOfExperience");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

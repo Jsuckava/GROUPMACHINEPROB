c11fdc6094ff4604c020eb1b8d84ceeea87cab0e
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-09-21 18:42:49 +0800

    Add files via upload

---
+++ CookBookRecipeDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachineProblem1.Models;
+
+public partial class CookBookRecipeDbContext : DbContext
+{
+    public CookBookRecipeDbContext()
+    {
+    }
+
+    public CookBookRecipeDbContext(DbContextOptions<CookBookRecipeDbContext> options)
+        : base(options)
+    {
+    }
+    public virtual DbSet<CookBookRecipe_Tbl> RecipesTB { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CookBook_RecipeDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CookBookRecipe_Tbl.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MachineProblem1.Models
+{
+    public class CookBookRecipe_Tbl
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Food name is required.")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Food name must be between 2 and 200 characters.")]
+        public string Food_Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Ingredients are required.")]
+        [MinLength(5, ErrorMessage = "Ingredients must be at least 5 characters long.")]
+        public string Ingredients { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Instructions are required.")]
+        [MinLength(10, ErrorMessage = "Instructions must be at least 10 characters long.")]
+        public string Instructions { get; set; } = string.Empty;
+    }
+}

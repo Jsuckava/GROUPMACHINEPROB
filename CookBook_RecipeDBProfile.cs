318ad73008c9cc1cc06cb995b6302d810fed5c86
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-09-21 18:43:10 +0800

    Add files via upload

---
+++ CookBook_RecipeDBProfile.cs
@@ -0,0 +1,14 @@
+﻿using MachineProblem1.Models;
+using MachineProblem1.DTO;
+using AutoMapper;
+namespace MachineProblem1.Profiles
+{
+    public class CookBook_RecipeDBProfile : Profile
+    {
+        public CookBook_RecipeDBProfile() {
+            CreateMap<CookBookRecipe_Tbl, CookBook_RecipeReadMapper>();
+            CreateMap<CookBook_RecipeCreateMapper, CookBookRecipe_Tbl>();
+            CreateMap<CookBook_RecipeUpdateMapper, CookBookRecipe_Tbl>();
+        }
+    }
+}

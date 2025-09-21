a60afa5b9fb9a134577eb2feb4d386024fa030ce
Jsuckava <147977927+Jsuckava@users.noreply.github.com>
2025-09-21 18:42:10 +0800

    Add files via upload

---
+++ CookBook_RecipeDB_Mapper.cs
@@ -0,0 +1,26 @@
+﻿namespace MachineProblem1.DTO
+{
+    public class CookBook_RecipeReadMapper
+    {
+        public string Food_Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Ingredients { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
+    }
+
+    public class CookBook_RecipeCreateMapper
+    {
+        public string Food_Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Ingredients { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
+    }
+
+    public class CookBook_RecipeUpdateMapper
+    {
+        public string Food_Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Ingredients { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
+    }
+}
